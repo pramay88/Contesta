bf5e1fb090e3b5b579a4f79849342840ae2ea1d7
Pramay Wankhade <pramaywankhade7@gmail.com>
2026-07-18 23:24:06 +0530

    feat: Add MS Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { AppShell } from "@/components/AppShell";
+import Script from "next/script";
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -112,6 +114,20 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);
+        t.async=1;
+        t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "xohml386hg");
+    `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}>
         <AppShell>{children}</AppShell>
         <script
